3127d6f7ea7970f39de304385f882f47450d25ed
Muhammad Ali Usman <muh.aliusman@yahoo.co.id>
2021-02-01 21:34:40 +0700

    Title sidebar click

--- resources/views/components/title/sidebar.blade.php
+++ resources/views/components/title/sidebar.blade.php
@@ -1,6 +1,10 @@
+@php
+$rootComponent = config('larawire.root_component');
+@endphp
 <a
-	class="h-full block text-center pl-3 pt-2 text-lg font-bold text-gray-800"
-	href="{{ env('APP_URL') }}"
+	class="h-full block text-center pl-3 pt-2 text-lg font-bold text-gray-800 cursor-pointer"
+	wire:click.prevent="$emit('changeComponent', '{{ $rootComponent }}')"
+	x-on:click="pushStateUrl('{{ $rootComponent }}')"
 >
   <img class="w-36 h-auto" src="{{ asset('images/larawire.png') }}">
 </a>
